e2a27ca0461963a0a419a974a13e5cf98ea1ec68
c-porto <carlos.portof@hotmail.com>
2023-06-22 16:55:13 -0300

    Single file setup, also json config

---
+++ config/config.py
@@ -0,0 +1,13 @@
+import json
+
+def load_config(module:str) -> dict:
+    with open("./config.json", "r") as config_file:
+        config:dict = json.load(config_file)
+    baudrate = config["baudrate"]
+    path_file = config["modules"][module]["path_file"]
+    path_srport = config["modules"][module]["path_sr_port"]
+    return {"baudrate": baudrate, "path_srport": path_srport, "path_file": path_file}
+
+if __name__ == "__main__":
+    config = load_config("EPS")
+    print(config["baudrate"])
---
+++ eps.py
@@ -0,0 +1,48 @@
+# SCRIPT TO RUN
+
+import time # Basically used to get local time
+import config.config as cfg
+import log_handlers.log_handler as log # Contains logic to detect important logs
+import serial.read_serial as sr # Contains the logic to read from serial port
+
+config:dict = cfg.load_config("EPS")
+
+while True:
+    # Start reading Serial Port for all modules
+    buffer = sr.serial_connection(port=config["path_sr_port"],baudrate=config["baudrate"])
+    
+    # Creating an iterable container to simplify going through all data
+
+    # Check for critical messages 
+    code = log.log_handler(buffer)
+    # Match for the type of the critical messages
+    # TODO Match of possible codes
+    if code == log.CODE_TYPE.OK:
+        continue
+    elif code == log.CODE_TYPE.ERROR:
+        log_date = time.strftime("[%d/%m/%Y] - %H:%M:%S")
+        file_date = time.strftime("[%d/%m/%Y]")
+        try:
+            path = ("{}/log-{}.txt".format(config["path_file"],file_date))
+            log_file = open(path,"a+")
+            log_file.write("{} - CODE::ERROR -> {}".format(log_date,buffer))
+            print("{} - CODE::ERROR -> {}".format(log_date,buffer))
+            log_file.close()
+        except (FileNotFoundError, PermissionError):
+            print("Error either on closing the file or creating a new one")
+        except (OSError, IOError):
+            print("Error on writing to the file")
+    elif code == log.CODE_TYPE.RESET:
+        log_date = time.strftime("[%d/%m/%Y] - %H:%M:%S")
+        file_date = time.strftime("[%d/%m/%Y]")
+        try:
+            path = ("{}/log-{}.txt".format(config["path_file"],file_date))
+            log_file = open(path,"a+")
+            log_file.write("{} - CODE::RESET -> {}".format(log_date,buffer))
+            print("{} - CODE::RESET -> {}".format(log_date,buffer))
+            log_file.close()
+        except (FileNotFoundError, PermissionError):
+            print("Error either on closing the file or creating a new one")
+        except (OSError, IOError):
+            print("Error on writing to the file")
+        
