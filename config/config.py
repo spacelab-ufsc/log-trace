db9d4ca42a529288c5bb80d393549574fdeee1a5
c-porto <carlos.portof@hotmail.com>
2023-06-23 09:09:18 -0300

    Fixed config name

--- config/config.py
+++ config/config.py
@@ -6,5 +6,5 @@ def load_config(module:str) -> dict:
     baudrate = config["baudrate"]
     path_file = config["modules"][module]["path_file"]
     path_srport = config["modules"][module]["path_sr_port"]
-    return {"baudrate": baudrate, "path_srport": path_srport, "path_file": path_file}
+    return {"baudrate": baudrate, "path_sr_port": path_srport, "path_file": path_file}
 
