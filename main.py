21f5f8385c02e00e81081274dd77d74b0b54e34c
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-03-20 20:40:27 -0300

    Updating Log Trace to only save the logs while highlighting the errors.

--- main.py
+++ main.py
@@ -1,51 +1,73 @@
-# SCRIPT TO RUN
+import serial
+import argparse
+import re
+import logging
+import os
 
-import time  # Basically used to get local time
-import config.config as cfg
-import log_handlers.log_handler as log  # Contains logic to detect important logs
-import serial.read_serial as sr  # Contains the logic to read from serial port
+ERROR_CODE = "\033[1;31m"
+BAUD = 115200
+LOG_DIR = 'logs'
 
-print("Please select which module to log")
-module: str = input("Options are -> [OBDH,EPS,TTC] > ").upper().strip()
+def remove_ansi_color(string: str) -> str:
+    ansi_escape = re.compile(r'\x1b(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape.sub('', string)
 
-config: dict = cfg.load_config(module)
+def setup_logging(module: str, log_dir: str):
+    filename = "./" + log_dir + "/" + module + ".log"
 
-while True:
-    # Start reading Serial Port for all modules
-    buffer = sr.serial_connection(
-        port=config["path_sr_port"], baudrate=config["baudrate"]
-    )
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+        print(f'Creating log directory on: {log_dir}')
 
-    # Creating an iterable container to simplify going through all data
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            f.write("")  
+        print(f'Creating log file on: {filename}')
 
-    # Check for critical messages
-    code = log.log_handler(buffer)
-    # Match for the type of the critical messages
-    if code == log.CODE_TYPE.OK:
-        continue
-    if code == log.CODE_TYPE.ERROR:
-        log_date = time.strftime("[%d/%m/%Y] - %H:%M:%S")
-        file_date = time.strftime("[%d/%m/%Y]")
-        try:
-            path = f"{config['path_file']}/{module}-{file_date}.txt"
-            log_file = open(path, "a+", encoding="ascii")
-            log_file.write(f"{log_date} - CODE::ERROR -> {buffer}")
-            print(f"{log_date} on {module} - CODE::ERROR -> {buffer}")
-            log_file.close()
-        except (FileNotFoundError, PermissionError):
-            print("Error either on closing the file or creating a new one")
-        except (OSError, IOError):
-            print("Error on writing to the file")
-    if code == log.CODE_TYPE.RESET:
-        log_date = time.strftime("[%d/%m/%Y] - %H:%M:%S")
-        file_date = time.strftime("[%d/%m/%Y]")
-        try:
-            path = f"{config['path_file']}/{module}-{file_date}.txt"
-            log_file = open(path, "a+", encoding="ascii")
-            log_file.write(f"{log_date} - CODE::RESET -> {buffer}")
-            print(f"{log_date} on {module} - CODE::RESET -> {buffer}")
-            log_file.close()
-        except (FileNotFoundError, PermissionError):
-            print("Error either on closing the file or creating a new one")
-        except (OSError, IOError):
-            print("Error on writing to the file")
+    logging.basicConfig(filename=filename, level=logging.DEBUG,
+                    format='[%(asctime)s][%(levelname)s] > %(message)s')
+
+def save_logs(line: str):
+    logline = remove_ansi_color(line)
+
+    if ERROR_CODE in line:
+        logging.error(logline)
+    else:
+        logging.info(logline)
+
+def serial_connection(port, baudrate): 
+    stream = serial.Serial(port=port, baudrate=baudrate)
+    return stream
+
+def serial_read(stream) -> str:
+    b_buffer = stream.readline()
+    return b_buffer.decode("ascii")
+
+def log_trace_cli():
+    cli_parser = argparse.ArgumentParser(prog="Log Trace",description="Reads the floripasat-2 modules log through UART")
+    cli_parser.add_argument('PORT', type=str)
+    cli_parser.add_argument('-m', '--module', action='store', default="module", type=str, help='name of the file to save the logs')
+    cli_parser.add_argument('-l', '--log_dir', action='store', default=LOG_DIR, type=str, help='Sets the log directory')
+
+    args = cli_parser.parse_args()
+
+    setup_logging(args.module, args.log_dir)
+
+    dev = serial_connection(args.port, BAUD)
+
+    try:
+        while True:
+            log_line = serial_read(dev)
+            print(log_line)   
+            save_logs(log_line)
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Exiting...")
+
+    finally:
+        dev.close()
+
+if __name__ == "__main__":
+    log_trace_cli()
+
+
